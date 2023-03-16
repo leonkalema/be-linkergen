feb838ab9ecc05093fd208644abfe4802ef6d41c
leonkalema <tullapeople@gmail.com>
2023-03-16 05:37:12 +0100

    backend service

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const useragent = require('useragent');
+
+const app = express();
+app.use(bodyParser.json());
+
+const urlDatabase = new Map();
+
+app.post('/api/generate', (req, res) => {
+  const { websiteUrl, appleStoreUrl, androidStoreUrl } = req.body;
+  const uniqueId = crypto.randomBytes(8).toString('hex');
+  const generatedUrl = `https://yourdomain.com/redirect/${uniqueId}`;
+
+  urlDatabase.set(uniqueId, { websiteUrl, appleStoreUrl, androidStoreUrl });
+
+  res.json({ generatedUrl });
+});
+
+app.get('/redirect/:id', (req, res) => {
+  const { id } = req.params;
+  const urls = urlDatabase.get(id);
+
+  if (!urls) {
+    res.status(404).send('URL not found');
+    return;
+  }
+
+  const userAgent = useragent.parse(req.headers['user-agent']);
+  const isMobile = userAgent.device && userAgent.device.type === 'mobile';
+  const isIos = isMobile && userAgent.os.family === 'iOS';
+  const isAndroid = isMobile && userAgent.os.family === 'Android';
+
+  if (isIos) {
+    res.redirect(urls.appleStoreUrl);
+  } else if (isAndroid) {
+    res.redirect(urls.androidStoreUrl);
+  } else {
+    res.redirect(urls.websiteUrl);
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
